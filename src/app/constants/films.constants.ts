2328346e13aaceb32ba78a7b0bf61dea6641322e
Шишковский Вадим Станиславович <vadzim.shyshkouski@b-logic.by>
2025-09-24 11:07:49 +0300

    Initial commit Angular netclicks

---
+++ src/app/constants/films.constants.ts
@@ -0,0 +1,30 @@
+import { Film } from "../models/film.model";
+export const FILMS: Film[] = [
+    {
+        id: 0,
+        title: 'Марсианин1',
+        original: 'The Martian 2015',
+        category: 'Фантастика, приключения',
+        rating: 7.8,
+        link: './film.html',
+        image: './images/film.png'
+    },
+    {
+        id: 1,
+        title: 'Марсианин2',
+        original: 'The Martian 2015',
+        category: 'Фантастика, приключения',
+        rating: 7.8,
+        link: './film.html',
+        image: './images/film.png'
+    },
+    {
+        id: 2,
+        title: 'Марсианин3',
+        original: 'The Martian 2015',
+        category: 'Фантастика, приключения',
+        rating: 7.8,
+        link: './film.html',
+        image: './images/film.png'
+    }
+]
